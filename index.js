808764ef17b52f8a57172731f17f557cdfc275a3
umesh <umesharma.us11@gmail.com>
2022-05-22 11:06:56 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,56 @@
 let btn = document.querySelectorAll(".drum");
 
-console.log(btn)
+// console.log(btn)
 
 
 for (let i = 0; i < btn.length; i++){
-    document.querySelectorAll(".drum")[i].addEventListener('click',
-        () => {console.log("I got clicked")
-    btn[i].style.color = "green";})
-    
+    document.querySelectorAll(".drum")[i].addEventListener('click',function ()  {
+            // console.log("I got clicked")
+            
+        this.style.color = "white";
+        console.log(this.innerHTML)
+        switch (this.innerHTML) {
+            case "w":
+                let tom1 = new Audio("./sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "a":
+                let tom2 = new Audio("./sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "s":
+                let tom3 = new Audio("./sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "d":
+                let tom4 = new Audio("./sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                let crash = new Audio("./sounds/crash.mp3");
+                crash.play();
+                break;
+            case "k":
+                let kick = new Audio("./sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            case "l":
+                let snare = new Audio("./sounds/snare.mp3");
+                snare.play();
+                break;
+            // case "w":
+            //     let audio = new Audio("./sounds/tom-1.mp3");
+            //     audio.play();
+            //     break
+
+           }
+       
+        
+        })
+  
 
 }
 
 
-
+ //       let audio = new Audio("./sounds/tom-1.mp3");
+    // audio.play();
