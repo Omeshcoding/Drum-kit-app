639c4cd209527ba06ab2cc450f0cebf35e324893
umesh <umesharma.us11@gmail.com>
2022-05-22 12:35:30 +0530

    initial commit

--- index.js
+++ index.js
@@ -6,10 +6,30 @@ let btn = document.querySelectorAll(".drum");
 for (let i = 0; i < btn.length; i++){
     document.querySelectorAll(".drum")[i].addEventListener('click',function ()  {
             // console.log("I got clicked")
-            
-        this.style.color = "white";
-        console.log(this.innerHTML)
-        switch (this.innerHTML) {
+        let audioBtn = this.innerHTML
+        
+        soundPlay(audioBtn);
+        BtnAnimation(audioBtn);
+       
+        })
+}
+
+document.addEventListener("keydown", (event) =>{
+    
+    // let value = e;
+    soundPlay(event.key);
+    BtnAnimation(event.key);
+    console.log(event)
+});
+ //       let audio = new Audio("./sounds/tom-1.mp3");
+    // audio.play();
+
+
+function soundPlay(key) {
+        
+    //   this.style.color = "white";
+       
+        switch (key) {
             case "w":
                 let tom1 = new Audio("./sounds/tom-1.mp3");
                 tom1.play();
@@ -42,15 +62,18 @@ for (let i = 0; i < btn.length; i++){
             //     let audio = new Audio("./sounds/tom-1.mp3");
             //     audio.play();
             //     break
-
-           }
-       
-        
-        })
+            
   
+           }
 
 }
-
-
- //       let audio = new Audio("./sounds/tom-1.mp3");
-    // audio.play();
+    
+function BtnAnimation(currentKey) {
+     let activeBtn = document.querySelector("."+ currentKey)   
+     
+    activeBtn.classList.add("pressed")
+    setTimeout(function () {
+        activeBtn.classList.remove("pressed")
+        
+    }, 100);
+}
